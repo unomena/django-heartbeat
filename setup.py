a97b87a2c1d698183a194f61bd6c15be59e9c3b9
Thys <thys@unomena.com>
2017-01-13 11:16:56 +0200

    Upped the version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-heartbeat',
-    version='0.0.2',
+    version='0.0.3',
     description='Django Heartbeat Application',
     author='Unomena',
     author_email='dev@unomena.com',

72459a5a4b0709d9ffec83a8b778e868981a2104
Euan Jonker <euan@unomena.com>
2017-03-06 15:34:55 +0200

    Fixed the management command to use proper argument parser.

--- heartbeat/management/commands/heartbeat.py
+++ heartbeat/management/commands/heartbeat.py
@@ -6,11 +6,13 @@ from django.conf import settings
 
 
 class Command(BaseCommand):
-    args = 'up | down'
-    help = 'Flag this server for maintenance - this stops the LB from sending new requests this way'
+
+    def add_arguments(self, parser):
+        parser.add_argument('state', type=str)
 
     def handle(self, *args, **options):
-        if 'up' in args:
+        if options['state'] == 'up':
+            print('Making heartbeat up')
             try:
                 if os.path.exists(settings.HEARTBEAT_FILENAME):
                     os.unlink(settings.HEARTBEAT_FILENAME)
@@ -22,7 +24,8 @@ class Command(BaseCommand):
             except:
                 raise CommandError('FAILED to delete the heartbeat file: %s' % traceback.format_exc())
 
-        if 'down' in args:
+        if options['state'] == 'down':
+            print('Making heartbeat down')
             try:
                 open(settings.HEARTBEAT_FILENAME, 'w').write('0')
 
